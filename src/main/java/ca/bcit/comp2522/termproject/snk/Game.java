d4ec73398d8928eccd880dea88021348026c8b0e
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-03-22 11:28:49 -0700

    Store all destructions, bullets, and aliens inside of lists

--- src/main/java/ca/bcit/comp2522/termproject/snk/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/snk/Game.java
@@ -1,26 +1,33 @@
 package ca.bcit.comp2522.termproject.snk;
 
+import java.util.ArrayList;
+
 public class Game {
 
         private final PlayerShip playerShip;
-        private final Destruction destruction;
-        private final Bullet bullet;
+        private final ArrayList<Destruction> destructions;
+        private final ArrayList<Bullet> bullets;
+        private final ArrayList<Alien> aliens;
 
         public Game() {
             playerShip = new PlayerShip();
-            destruction = new Destruction();
-            bullet = new Bullet();
+            destructions = new ArrayList<>();
+            bullets = new ArrayList<>();
+            aliens = new ArrayList<>();
         }
 
         public PlayerShip getPlayerShip() {
             return playerShip;
         }
 
-        public Destruction getDestruction() {
-            return destruction;
+        public ArrayList<Destruction> getDestruction() {
+            return destructions;
         }
 
-        public Bullet getBullet() {
-            return bullet;
+        public ArrayList<Bullet> getBullet() {
+            return bullets;
+        }
+        public ArrayList<Alien> getAliens() {
+            return aliens;
         }
 }
