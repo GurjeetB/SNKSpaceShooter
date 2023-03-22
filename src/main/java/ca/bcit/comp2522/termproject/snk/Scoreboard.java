ae5ced2f5c595ed390d89755cea68320fb3f39fb
anothersam22 <113322947+anothersam22@users.noreply.github.com>
2023-03-22 11:12:11 -0700

    add score board class methods

--- src/main/java/ca/bcit/comp2522/termproject/snk/Scoreboard.java
+++ src/main/java/ca/bcit/comp2522/termproject/snk/Scoreboard.java
@@ -1,4 +1,42 @@
 package ca.bcit.comp2522.termproject.snk;
 
 public class Scoreboard {
+
+        private int score;
+        private int highScore;
+
+        public Scoreboard() {
+            score = 0;
+            highScore = 0;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        public int getHighScore() {
+            return highScore;
+        }
+
+        public void setHighScore(int highScore) {
+            this.highScore = highScore;
+        }
+
+        public void updateScore(int score) {
+            this.score += score;
+        }
+
+        public void updateHighScore() {
+            if (score > highScore) {
+                highScore = score;
+            }
+        }
+
+        public void resetScore() {
+            score = 0;
+        }
 }
