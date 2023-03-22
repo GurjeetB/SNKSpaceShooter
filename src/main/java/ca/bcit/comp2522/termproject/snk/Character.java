2b56c2915b375284c87ed8f85a0a3728a3303bf2
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-03-22 11:16:18 -0700

    Turned Character into an abstract class, created variables & methods

--- src/main/java/ca/bcit/comp2522/termproject/snk/Character.java
+++ src/main/java/ca/bcit/comp2522/termproject/snk/Character.java
@@ -1,4 +1,35 @@
 package ca.bcit.comp2522.termproject.snk;
 
-public interface Character {
+public abstract class Character {
+    /**
+     * The character's health.
+     */
+    int health;
+    /**
+     * The character's horizontal position.
+     */
+    int positionX;
+
+    /**
+     * Creates a bullet at the Character's position.
+     * @return A new instance of a bullet
+     */
+    abstract Bullet shoot();
+
+    /**
+     * Moves the Character left, then returns its new horizontal position.
+     * @return The character's new horizontal position
+     */
+    abstract int moveLeft();
+    /**
+     * Moves the Character left, then returns its new horizontal position.
+     * @return The character's new horizontal position
+     */
+    abstract int moveRight();
+
+    /**
+     * Creates an Explosion at the Character's position.
+     * @return A new instance of an Explosion
+     */
+    abstract Destruction explode();
 }
