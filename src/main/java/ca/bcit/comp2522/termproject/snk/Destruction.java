69e74ccbc2bf58fd8088020f580d7ecba37152e7
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-03-22 11:26:09 -0700

    Added constructor and getter

--- src/main/java/ca/bcit/comp2522/termproject/snk/Destruction.java
+++ src/main/java/ca/bcit/comp2522/termproject/snk/Destruction.java
@@ -1,4 +1,13 @@
 package ca.bcit.comp2522.termproject.snk;
 
 public class Destruction {
+    final String explosionType;
+
+    public Destruction(String explosionType) {
+        this.explosionType = explosionType;
+    }
+
+    public String getExplosionType() {
+        return explosionType;
+    }
 }
