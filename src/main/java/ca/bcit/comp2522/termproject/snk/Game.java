e5146362e5d2401ce321febab8d3637dd7bd14af
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-03-23 14:43:22 -0700

    Add test scenario for onKeyPress

--- src/main/java/ca/bcit/comp2522/termproject/snk/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/snk/Game.java
@@ -1,19 +1,29 @@
 package ca.bcit.comp2522.termproject.snk;
 
+import javafx.scene.input.KeyEvent;
+
 import java.util.ArrayList;
 
 public class Game {
-
+        private static Game gameSingleton;
+        public static Game getInstance() {
+            if (gameSingleton == null) {
+                gameSingleton = new Game();
+            }
+            return gameSingleton;
+        }
         private final PlayerShip playerShip;
         private final ArrayList<Destruction> destructions;
         private final ArrayList<Bullet> bullets;
         private final ArrayList<Alien> aliens;
+        private boolean move;
 
-        public Game() {
+        private Game() {
             playerShip = new PlayerShip(100, 320, 320);
             destructions = new ArrayList<>();
             bullets = new ArrayList<>();
             aliens = new ArrayList<>();
+            move = false;
         }
 
         public PlayerShip getPlayerShip() {
@@ -29,5 +39,22 @@ public class Game {
         }
         public ArrayList<Alien> getAliens() {
             return aliens;
+        }
+
+        void onKeyPressed(KeyEvent event) {
+            switch (event.getCode()) {
+                case A -> {
+                    move = true;
+                }
+                case D -> {
+                    move = false;
+                }
+            }
+        }
+        public void runAllGameLogic() {
+            // Runs all possible game logic
+            if (move) {
+                System.out.println(move);
+            }
         }
 }
--- src/main/java/ca/bcit/comp2522/termproject/snk/GameScreenController.java
+++ src/main/java/ca/bcit/comp2522/termproject/snk/GameScreenController.java
@@ -1,4 +1,29 @@
 package ca.bcit.comp2522.termproject.snk;
 
-public class GameScreenController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.security.Key;
+import java.util.ResourceBundle;
+
+public class GameScreenController implements Initializable {
+    Game game = Game.getInstance();
+    ClassThatRunsAFunctionConstantly testClass = new ClassThatRunsAFunctionConstantly() {
+        @Override
+        public void tick(float secondsSinceLastFrame) {
+            game.runAllGameLogic();
+        }
+    };
+
+    @FXML
+    public void onKeyPressed(KeyEvent event) {
+        game.onKeyPressed(event);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        testClass.start();
+    }
 }
