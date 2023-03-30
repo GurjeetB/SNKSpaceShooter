53242c42722f0b56632ca926a6d6aee72a1d9a81
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-03-30 11:44:13 -0700

    Addded cooldown before shooting bullet

--- src/main/java/ca/bcit/comp2522/termproject/snk/Alien.java
+++ src/main/java/ca/bcit/comp2522/termproject/snk/Alien.java
@@ -1,13 +1,26 @@
 package ca.bcit.comp2522.termproject.snk;
 
 public class Alien extends Character {
-
+    private static final int DEFAULT_FIRE_COOLDOWN = 100;
+    private int cooldownBeforeShooting;
     public Alien(int health, int positionX, int positionY) {
         super(health, positionX, positionY);
+        cooldownBeforeShooting = DEFAULT_FIRE_COOLDOWN;
+    }
+
+    public boolean isReadyToFire() {
+        if (cooldownBeforeShooting == 0) {
+            cooldownBeforeShooting = DEFAULT_FIRE_COOLDOWN;
+            return true;
+        } else {
+            cooldownBeforeShooting -= 1;
+            return false;
+        }
     }
 
     @Override
     Bullet shoot() {
+        System.out.println("BOOM!");
         return new Bullet();
     }
 
