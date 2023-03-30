05295930342388e3962fdbea68baf83fb406ef47
GurjeetB <gurjeetbhangoo@yahoo.com>
2023-03-30 11:45:14 -0700

    Created GraphicsEngine for rendering in JavaFX

---
+++ src/main/java/ca/bcit/comp2522/termproject/snk/GraphicsEngine.java
@@ -0,0 +1,39 @@
+package ca.bcit.comp2522.termproject.snk;
+
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
+import java.util.ArrayList;
+
+public class GraphicsEngine {
+    private static GraphicsEngine singleton;
+    public static GraphicsEngine getInstance(AnchorPane gameRoot) {
+        if (singleton == null) {
+            singleton = new GraphicsEngine(gameRoot);
+        }
+        return singleton;
+    }
+    private final ObservableList<Node> allEntities;
+    private GraphicsEngine(AnchorPane gameRoot) {
+        allEntities = gameRoot.getChildren();
+    }
+
+    public void refreshScreen() {
+        allEntities.clear();
+    }
+
+    public void renderPlayer(PlayerShip playerShip) {
+        Circle playerSprite = new Circle(playerShip.getPositionX(),
+                playerShip.getPositionY(), 15);
+        allEntities.add(playerSprite);
+    }
+
+    public void renderAliens(ArrayList<Alien> alienList) {
+        alienList.forEach(alien -> {
+            Circle alienSprite = new Circle(alien.getPositionX(), alien.getPositionY(), 20);
+            allEntities.add(alienSprite);
+        });
+    }
+}
